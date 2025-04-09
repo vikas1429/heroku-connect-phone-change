1bef01a10f46f9e6fb569b2a835706fdc3185dc4
vikas1429 <vikas1429@gmail.com>
2025-04-09 12:55:40 +1000

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var pg = require('pg');
 
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 5000);
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
